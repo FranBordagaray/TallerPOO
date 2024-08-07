65eef59c142a66ffc62f3380007ef79713bd009f
SofiaGarbarini1 <sofiagarbarini19@gmail.com>
2024-08-07 11:53:57 -0300

    comienzo pantalla historial

---
+++ DeliciasGourmet/src/Vista/Historial.java
@@ -0,0 +1,17 @@
+package Vista;
+
+import javax.swing.JPanel;
+
+public class Historial extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public Historial() {
+		setLayout(null);
+
+	}
+
+}
