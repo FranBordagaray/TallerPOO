23c8c983f677ea3837b5b8f930364b508885c809
SofiaGarbarini1 <sofiagarbarini19@gmail.com>
2024-09-04 11:10:19 -0300

    Modificaciones Historial y UsarTarjeta

--- DeliciasGourmet/src/Vista/Historial.java
+++ DeliciasGourmet/src/Vista/Historial.java
@@ -5,20 +5,20 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+
 public class Historial extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 
 
 	private JTable tabla;
-	private DefaultTableModel model;
+	private DefaultTableModel modelo;
 	private Object[][] datos; 
 	public Historial() {
 		setBorder(null);
@@ -33,16 +33,24 @@ public class Historial extends JPanel {
 			{ "15-03-2022", "12:00", "Reservado", "Mesa 5", 2 },
 			{ "17-08-2023", "21:30", "Cancelado", "Mesa 3", 6 },
 			{ "25-05-2024", "20:00", "Finalizado", "Mesa 7", 1 }, 
+			
 		};
 
-		// Crear el modelo de la tabla
-		model = new DefaultTableModel(datos, columna);
-		tabla = new JTable(model);
+	     modelo = new DefaultTableModel(datos, columna) {
+	            @Override
+	            public boolean isCellEditable(int fila, int columna) {
+	                return false; 
+	            }
+	        };
 
+	        tabla = new JTable(modelo);
+
+	    //Panel de tabla
 		JScrollPane scrollPane = new JScrollPane(tabla);
 		scrollPane.setBounds(10, 73, 951, 226);
 		add(scrollPane);
 		
+		//Panel Superior
 		JPanel pnlHeader = new JPanel();
 		pnlHeader.setBorder(null);
 		pnlHeader.setBackground(new Color(195, 155, 107));
@@ -50,16 +58,18 @@ public class Historial extends JPanel {
 		add(pnlHeader);
 		pnlHeader.setLayout(null);
 		
+		//Titulo Historial
 		JLabel lblHistorial = new JLabel("HISTORIAL DE RESERVAS");
 		lblHistorial.setFont(new Font("Roboto Light", Font.BOLD, 20));
 		lblHistorial.setBounds(98, 19, 248, 24);
 		pnlHeader.add(lblHistorial);
 		
+		//Filtro Estado
 		JLabel lblFiltro1 = new JLabel("Filtrar estados:");
 		lblFiltro1.setBounds(10, 54, 86, 14);
 		add(lblFiltro1);
 
-		// seleccion de filtro de estados 
+		// Seleccion de Filtro de Estados 
 		String[] estados = { "Todos", "Reservado", "Cancelado", "Finalizado" };
 		JComboBox<String> comboBox = new JComboBox<>(estados);
 		comboBox.setBackground(Color.WHITE);
@@ -77,13 +87,13 @@ public class Historial extends JPanel {
 		});
 	}
 
-	
+	//Metodo que filtra por estado
 	private void filtrarEstado(String estado) {
 		if (estado.equals("Todos")) {
-			model.setDataVector(datos, new String[] { "Fecha", "Hora", "Estado", "Mesa", "Ocupantes" });
+			modelo.setDataVector(datos, new String[] { "Fecha", "Hora", "Estado", "Mesa", "Ocupantes" });
 		} else {
 			Object[][] datosFiltrados = filtrarEstado(datos, estado);
-			model.setDataVector(datosFiltrados, new String[] { "Fecha", "Hora", "Estado", "Mesa", "Ocupantes" });
+			modelo.setDataVector(datosFiltrados, new String[] { "Fecha", "Hora", "Estado", "Mesa", "Ocupantes" });
 		}
 	}
 
